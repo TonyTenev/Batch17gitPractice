aa9924b63fd3040da058da01ebfd654dda5f2c3a
TonyTenev <tentchotenev@gmail.com>
2023-09-18 18:34:41 -0500

    this is our first commit

---
+++ src/main/java/gitPractice/Practice.java
@@ -0,0 +1,14 @@
+package gitPractice;
+
+public class Practice {
+    public static void main(String[] args) {
+
+        System.out.println("ebane");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*");
+            System.out.println("**");
+        }
+
+    }
+}
